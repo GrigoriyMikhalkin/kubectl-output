b165aae39b90ec624c00f21a7277d6ca3187951e
GrigoriyMikhalkin <grigoriymikhalkin@gmail.com>
2024-06-01 20:13:13 +0200

    set command resolves resource name to it's full form

--- pkg/plugin/set.go
+++ pkg/plugin/set.go
@@ -5,7 +5,12 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"io"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
+	"slices"
+	"strings"
 )
 
 type ResourceTmpMap map[string]*Resource
@@ -31,6 +36,8 @@ func RunSetCmd(resource, tmpName, tmp, namespace string, overwrite, setDefault b
 		err error
 		f   *os.File
 	)
+
+	fullResourceName := getFullResourceName(resource)
 
 	_, err = os.Stat("~/.kube-output/resource_tmp_map.yaml")
 	if os.IsNotExist(err) {
@@ -65,14 +72,14 @@ func RunSetCmd(resource, tmpName, tmp, namespace string, overwrite, setDefault b
 	rtmap := ResourceTmpMap{}
 	yaml.Unmarshal(buf.Bytes(), &rtmap)
 
-	r := rtmap[resource]
+	r := rtmap[fullResourceName]
 	if r == nil {
 		r = &Resource{
 			Default:    "",
 			Namespaces: map[string]string{},
 			Templates:  map[string]string{},
 		}
-		rtmap[resource] = r
+		rtmap[fullResourceName] = r
 	}
 
 	if r.Templates[tmpName] != "" {
@@ -106,4 +113,113 @@ func RunSetCmd(resource, tmpName, tmp, namespace string, overwrite, setDefault b
 	if err != nil {
 		panic(err)
 	}
+}
+
+func getFullResourceName(resource string) string {
+	// Should split resource by '.' to extract type, version and group if available
+	var t, v, g string
+	resourceParts := strings.Split(resource, ".")
+	switch len(resourceParts) {
+	case 1:
+		t = resourceParts[0]
+	case 2:
+		t, g = resourceParts[0], resourceParts[1]
+	case 3:
+		t, v, g = resourceParts[0], resourceParts[1], resourceParts[2]
+	default:
+		panic(fmt.Errorf("invalid resource name: %s", resource))
+	}
+
+	groups, resources := discoverAPIResources()
+
+	groupVersions := make(map[string]struct{})
+	if g != "" {
+		for _, group := range groups {
+			if group.Name == g {
+				if v != "" {
+					for _, version := range group.Versions {
+						if version.Version == v {
+							groupVersions[version.GroupVersion] = struct{}{}
+							break
+						}
+					}
+				} else {
+					for _, version := range group.Versions {
+						groupVersions[version.GroupVersion] = struct{}{}
+					}
+				}
+				break
+			}
+		}
+	}
+
+	var fullName string
+	for _, resourceList := range resources {
+		if len(groupVersions) > 0 {
+			if _, ok := groupVersions[resourceList.GroupVersion]; ok {
+				for _, r := range resourceList.APIResources {
+					matches := r.Name == t || r.SingularName == t || slices.Contains(r.ShortNames, t)
+					if matches {
+						if fullName != "" {
+							panic(fmt.Errorf("resource name %s is too ambiguous", t))
+						}
+
+						gv := strings.Split(resourceList.GroupVersion, "/")
+						if len(gv) == 1 {
+							// Means that group is not specified
+							fullName = r.Name
+						} else {
+							fullName = fmt.Sprintf("%s.%s.%s", r.Name, gv[1], gv[0])
+						}
+						break
+					}
+				}
+			}
+		} else {
+			for _, r := range resourceList.APIResources {
+				matches := r.Name == t || r.SingularName == t || slices.Contains(r.ShortNames, t)
+				if matches {
+					if fullName != "" {
+						panic(fmt.Errorf("resource name %s is too ambiguous", t))
+					}
+					gv := strings.Split(resourceList.GroupVersion, "/")
+					if len(gv) == 1 {
+						// Means that group is not specified
+						fullName = r.Name
+					} else {
+						fullName = fmt.Sprintf("%s.%s.%s", r.Name, gv[1], gv[0])
+					}
+					break
+				}
+			}
+		}
+
+	}
+
+	return fullName
+}
+
+func discoverAPIResources() ([]*v1.APIGroup, []*v1.APIResourceList) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	kubeconfig := fmt.Sprintf("%s/.kube/config", home)
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	groups, resources, err := clientset.Discovery().ServerGroupsAndResources()
+	if err != nil {
+		panic(err)
+	}
+
+	return groups, resources
 }
